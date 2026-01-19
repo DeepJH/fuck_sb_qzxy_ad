e53b8a874c78e78ada6f42f0e690e2470c14cc49
DeepJH <deepjhshenjing@outlook.com>
2026-01-19 17:50:56 +0800

    修正了脚本导致的规则生成错误

--- make_rules.sh
+++ make_rules.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat urls.txt | grep -v '^[[:space:]]*$' | awk '/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/ {print "  - IP-CIDR,"$1"/32,REJECT"} !/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/ {print "  - DOMAIN-SUFFIX,"$1",REJECT"}' > clash_rules.yaml
+grep -v '^\[source' urls.txt | tr -d '\r' | awk '{if ($1 ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) print "  - IP-CIDR,"$1"/32,REJECT"; else print "  - DOMAIN,"$1",REJECT"}' > clash_rules.yaml
 cat urls.txt | sed '/^[0-9]\{1,3\}\./s/^/||/ ; /^[0-9]\{1,3\}\./s/$/^/ ; /^[^0-9|]/s/^/||/ ; /^[^0-9|]/s/$/^/' > adguard_rules.txt
